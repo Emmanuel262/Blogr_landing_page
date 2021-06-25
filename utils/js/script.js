5bc2f541d54b0521abbbdb04b644465a883e17f2
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2021-06-25 10:01:36 +0200

    fixing bugs for better responsive design for mobile design

--- utils/js/script.js
+++ utils/js/script.js
@@ -1,4 +1,5 @@
 const nav_list = document.querySelectorAll('.list');
+const nav_two = document.querySelector('.nav_two');
 const sub_list = document.querySelectorAll('.sub_list');
 const openBtn = document.querySelector('.humberger');
 const navigationBox = document.querySelector('.nav_one');
@@ -9,11 +10,21 @@ nav_list.forEach((list) => {
         listIcon.classList.remove('fa-chevron-down');
         listIcon.classList.add('fa-chevron-up');
         const requiredList = list.lastElementChild;
+        nav_two.classList.add('active');
         if(requiredList.classList.contains('sub_list')) {
             requiredList.style.display = 'flex';
+            if(+window.innerWidth < 700) {
+                nav_two.style.marginTop = '8rem';
+            } else {
+                nav_two.style.marginTop = '0.1rem';
+            }
         }
-    })
+    });
+
 });
+
+console.log(window.innerWidth);
+
 nav_list.forEach((list) => {
     list.addEventListener('mouseleave', () => {
         const listIcon = list.querySelector('.fas');
@@ -22,13 +33,14 @@ nav_list.forEach((list) => {
         const requiredList = list.lastElementChild;
         if(requiredList.classList.contains('sub_list')) {
             requiredList.style.display = 'none';
+            nav_two.style.marginTop = '0.1rem';
         }
     })
 });
 
 openBtn.addEventListener('click', (e) => {
     if(e.target.classList.contains('humberger')) {
-        navigationBox.style.display = 'grid';
+        navigationBox.style.display = 'flex';
         openBtn.src = './utils/images/icon-close.svg';
         openBtn.classList.add('opened');
         e.target.addEventListener('click', (e) => {
@@ -37,7 +49,7 @@ openBtn.addEventListener('click', (e) => {
                 openBtn.src = './utils/images/icon-hamburger.svg';
                 openBtn.classList.remove('opened');
             } else {
-                navigationBox.style.display = 'grid';
+                navigationBox.style.display = 'flex';
                 openBtn.src = './utils/images/icon-close.svg';
                 openBtn.classList.add('opened');
             }
